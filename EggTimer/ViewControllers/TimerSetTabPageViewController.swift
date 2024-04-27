69681816975e3f1375c9e32bff1bcfc3a4a3240f
PochiApp <pochi.app.dp@gmail.com>
2024-04-28 00:40:07 +0900

    UIPageViewControllerを使った2画面スワイプ遷移を実装

--- EggTimer/ViewControllers/TimerSetTabPageViewController.swift
+++ EggTimer/ViewControllers/TimerSetTabPageViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 final class TimerSetTabPageViewController: UIPageViewController {
     
-    private var pageViewControllers: [UIViewController] = []
+    private var pageViewController: UIPageViewController!
+    private var controllers: [UIViewController] = []
     
     override func viewDidLoad() {
         
@@ -25,8 +26,31 @@ final class TimerSetTabPageViewController: UIPageViewController {
         let timerCountViewController = TimerCountViewController()
         timerCountViewController.view.backgroundColor = .yellow
         
-        self.pageViewControllers = [timerSetViewController, timerCountViewController]
+        self.controllers = [timerSetViewController, timerCountViewController]
         
+        setViewControllers([self.controllers[0]], direction: .forward, animated: false, completion: nil)
+        
+        self.dataSource = self
+    }
+}
+    
+    extension TimerSetTabPageViewController: UIPageViewControllerDataSource {
+        func pageViewController (_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            if let index = self.controllers.firstIndex(of: viewController),
+               index < self.controllers.count - 1 {
+                return self.controllers[index + 1]
+            } else {
+                return nil
+            }
+        }
+        
+        func pageViewController (_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            if let index = self.controllers.firstIndex(of: viewController),
+               index > 0 {
+                return self.controllers[index - 1]
+            } else {
+                return nil
+            }
+        }
     }
     
-}
